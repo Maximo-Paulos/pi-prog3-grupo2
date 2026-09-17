1483f872898760fef80d69c23d8e000c453ea54f
Maximo Paulos <161731319+Maximo-Paulos@users.noreply.github.com>
2026-09-17 12:41:58 -0300

    404 armado, faltan rutas y poner css en public

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -3,15 +3,19 @@ import { Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import NotFound from "./components/NotFound/NotFound";
 
 
 function App() {
   return (
     <div className="container-fluid d-flex flex-column min-vh-100">
-      <Header/>
+      <Header />
       <main className="flex-grow-1">
+        <Switch>
+          <Route component={NotFound} />
+        </Switch>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
