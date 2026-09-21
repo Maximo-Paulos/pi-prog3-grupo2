409732db24688d1dda6a41a11e59c811ba53167d
LeonMcCoubrey <lmccoubrey@udesa.edu.ar>
2026-09-20 21:07:30 -0300

    Agrego formulario del login y sus campos

--- proyecto/src/components/Login/Login.js
+++ proyecto/src/components/Login/Login.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Login() {
-  return (
-    <div className="container">
-      <h2>Login</h2>
-    </div>
-  );
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      error: ""
+    };
+  }
+
+  controlarEmail(event) {
+    this.setState({
+      email: event.target.value
+    });
+  }
+
+  controlarPassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  evitarSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={(event) => this.evitarSubmit(event)}>
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              id="email"
+              value={this.state.email}
+              onChange={(event) => this.controlarEmail(event)}
+            />
+          </div>
+          <div>
+            <label>Contraseña</label>
+            <input
+              type="password"
+              id="password"
+              value={this.state.password}
+              onChange={(event) => this.controlarPassword(event)}
+            />
+          </div>
+          <button type="submit">Iniciar sesión</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
