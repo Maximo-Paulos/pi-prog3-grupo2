42581dc230474bafa22fe911f24cb77c07140e17
Maximo Paulos <161731319+Maximo-Paulos@users.noreply.github.com>
2026-09-21 13:23:48 -0300

    creo carpeta screens

--- proyecto/src/screens/Home/home.js
+++ proyecto/src/screens/Home/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import MovieCard from "../Moviecard/MovieCard";
+import MovieCard from "../../components/Moviecard/MovieCard";
 
 class Home extends Component {
   constructor(props) {
